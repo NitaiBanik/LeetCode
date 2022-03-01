a11e80b10d3c121bfa1f563ad54705109fa50ba3
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-03-01 21:38:32 +0600

    Time: 0 ms (100.00%), Space: 5.8 MB (95.93%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
+    int calculateForPositiveNumber(int x, int limit){   
+        int reverseNumber = 0;
+        
+        while(x != 0){
+            int reminder = x % 10;
+            x/=10;
+
+            if(reverseNumber > limit) return 0;       
+            if(reverseNumber == limit && reminder > 7)  return 0;
+
+            reverseNumber = reverseNumber * 10 + reminder;
+        }
+        
+        return reverseNumber;
+    }
+    
     int reverse(int x) {
         if(x == 0) return 0;
-
-        int negativeLimit = 214748364;
-        int positiveLimit = 214748364;
+        
+        int limit = 214748364;
         
         if(x > 0){
-            int reverseNumber = 0;
-            while(x != 0){
-                int reminder = x % 10;
-                if(reverseNumber > positiveLimit) return 0;
-                if(reverseNumber == positiveLimit && reminder > 7)  return 0;
-                reverseNumber = reverseNumber * 10 + reminder;
-                x/=10;
-            }
-            return reverseNumber;
+            return calculateForPositiveNumber(x, limit);
         }
         else{
             if(x == -2147483648) return 0;
@@ -25,10 +32,10 @@ public:
                 int reminder = x % 10;
                 x/=10;
                 
-                if(reverseNumber > negativeLimit) return 0;
-                if(reverseNumber == negativeLimit && reminder > 8)  return 0;
+                if(reverseNumber > limit) return 0;
+                if(reverseNumber == limit && reminder > 8)  return 0;
                 
-                if(reverseNumber == negativeLimit && reminder < 9 && x == 0){
+                if(reverseNumber == limit && reminder < 9 && x == 0){
                     reverseNumber *= -1;
                     return reverseNumber * 10 - reminder;
                 }
