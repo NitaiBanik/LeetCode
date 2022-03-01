daafe1ef2a899d1a6046691a31db67dcd2f8fe52
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-03-01 21:48:32 +0600

    Time: 0 ms (100.00%), Space: 5.9 MB (32.73%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -18,25 +18,25 @@ public:
     
     int calculateForNegativeNumber(int x, int limit){   
         
-            if(x == -2147483648) return 0;
-        
-            x *= -1;
-            int reverseNumber = 0;
-        
-            while(x != 0){
-                int reminder = x % 10;
-                x/=10;
-                
-                if(reverseNumber > limit) return 0;
-                if(reverseNumber == limit && reminder > 8)  return 0;
-                
-                if(reverseNumber == limit && reminder < 9 && x == 0){
-                    reverseNumber *= -1;
-                    return reverseNumber * 10 - reminder;
-                }   
-                reverseNumber = reverseNumber * 10 + reminder;
-            }
-            return -1 * reverseNumber;
+        if(x == -2147483648) return 0;
+
+        x *= -1;
+        int reverseNumber = 0;
+
+        while(x != 0){
+            int reminder = x % 10;
+            x/=10;
+
+            if(reverseNumber > limit) return 0;
+            if(reverseNumber == limit && reminder > 8)  return 0;
+
+            if(reverseNumber == limit && reminder < 9 && x == 0){
+                reverseNumber *= -1;
+                return reverseNumber * 10 - reminder;
+            }   
+            reverseNumber = reverseNumber * 10 + reminder;
+        }
+        return -1 * reverseNumber;
     }
     
     int reverse(int x) {
