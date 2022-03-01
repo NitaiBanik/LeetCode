87ff63c8ad7b0daa663024fe1381031e672d3e1d
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-03-01 08:51:17 +0600

    Time: 4 ms (88.16%), Space: 7.9 MB (48.88%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
+    /// This is a pattern problem. After simulating on pen and paper pattern can be easily found.
+    /// If number is Even -> total ones of number, Ones[number] = Ones[number/2]
+    /// If Odd-> total ones of number, Ones[number] = Ones[number - 1] + 1
+    
     vector<int> countBits(int n) {
         
         vector<int>Ones(n + 1, 0);
         
-        for(int i = 0 ; i <= n; i++){
-            int numberOfOnes = 0;
-            
-            for(int bit = 0; bit <= 17; bit++){
-                if(i & (1 << bit)) numberOfOnes++;
-            }
-            Ones[i] = numberOfOnes;
+        Ones[0] = 0;
+        for(int number = 1 ; number <= n; number++){
+           if(number%2 == 0) Ones[number] = Ones[number/2];
+           else Ones[number] = Ones[number - 1] + 1;
         }
         
         return Ones;
