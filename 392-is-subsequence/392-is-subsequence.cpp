a5501860a280ee84b366ad5c16bad65a1ea83b9b
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-03-02 10:17:10 +0600

    Time: 6 ms (23.26%), Space: 6.7 MB (22.15%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,14 +1,48 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        if(s.size() == 0) return true;
         
-        int index = 0;
+        map<char, vector<int>> characterIndecies;
         
-        for(auto chr: t)
-            index += (chr == s[index] ? 1: 0);
+        for(int i = 0; i < t.size(); i++)
+            characterIndecies[t[i]].push_back(i);
         
+    /*    for(auto s: characterIndecies){
+            char ch = s.first;
+            vector<int> vec = s.second;
+            cout<<ch<<" positions: ";
+            for(auto id: vec) cout<<id<<" ";
+            cout<<endl;
+        }
+            */
         
-        return index == s.size();
+        int foundAt = -1; 
+
+        for(int i = 0; i < s.size(); i++){
+
+            if(characterIndecies.find(s[i]) == characterIndecies.end()){
+                cout<<" first Not Found: "<<s[i]<<endl;
+                return false;
+            }
+
+            vector<int> indicies = characterIndecies[s[i]];
+            cout<<"khujbo = "<<s[i]<<" Index Er pore = "<<foundAt<<endl;
+            
+            for(auto id: indicies)
+                cout<<id<<" ";
+            cout<<endl;
+
+            foundAt = lower_bound(indicies.begin(), indicies.end(), foundAt) - indicies.begin();
+            
+            cout<<"FoundAt "<<foundAt<<endl;
+
+            if((foundAt == indicies.size()) || (t[indicies[foundAt]] != s[i])){
+                cout<<"next Not Found: "<<s[i]<<endl;
+                return false;
+            }
+
+            foundAt = indicies[foundAt] + 1;
+        }
+        return true;
     }
 };
