3fba0c53bb0014dfbf74891b5433ed4c0fc80f54
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-03-02 10:24:49 +0600

    Time: 5 ms (28.86%), Space: 7.4 MB (12.54%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -1,48 +1,37 @@
 class Solution {
 public:
-    bool isSubsequence(string s, string t) {
-        
+     map<char, vector<int>> createCharacterIndicies(string t){
         map<char, vector<int>> characterIndecies;
-        
+         
         for(int i = 0; i < t.size(); i++)
             characterIndecies[t[i]].push_back(i);
-        
-    /*    for(auto s: characterIndecies){
-            char ch = s.first;
-            vector<int> vec = s.second;
-            cout<<ch<<" positions: ";
-            for(auto id: vec) cout<<id<<" ";
-            cout<<endl;
-        }
-            */
-        
+         
+         return characterIndecies;
+    }
+    
+    bool isFound(string s, string t,  map<char, vector<int>> characterIndecies){
         int foundAt = -1; 
-
         for(int i = 0; i < s.size(); i++){
 
-            if(characterIndecies.find(s[i]) == characterIndecies.end()){
-                cout<<" first Not Found: "<<s[i]<<endl;
+            if(characterIndecies.find(s[i]) == characterIndecies.end())
                 return false;
-            }
 
             vector<int> indicies = characterIndecies[s[i]];
-            cout<<"khujbo = "<<s[i]<<" Index Er pore = "<<foundAt<<endl;
-            
-            for(auto id: indicies)
-                cout<<id<<" ";
-            cout<<endl;
 
-            foundAt = lower_bound(indicies.begin(), indicies.end(), foundAt) - indicies.begin();
-            
-            cout<<"FoundAt "<<foundAt<<endl;
+            foundAt = lower_bound(indicies.begin(), indicies.end(), foundAt) - indicies.begin();    
 
-            if((foundAt == indicies.size()) || (t[indicies[foundAt]] != s[i])){
-                cout<<"next Not Found: "<<s[i]<<endl;
+            if((foundAt == indicies.size()) || (t[indicies[foundAt]] != s[i]))
                 return false;
-            }
 
             foundAt = indicies[foundAt] + 1;
         }
         return true;
+    }
+    
+    bool isSubsequence(string s, string t) {    
+        auto characterIndecies = createCharacterIndicies(t);
+    
+        return isFound(s, t, characterIndecies);
+        
     }
 };
