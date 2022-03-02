86b6035d7c20b1d4f6829b9da0d198c4c05d3c68
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-03-02 23:40:26 +0600

    Time: 6 ms (55.87%), Space: 12 MB (93.54%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-       
-        int leftIndex = -1, rightIndex = nums.size() - 1;
-        
+    void swapValues(int index1, int index2, vector<int>& nums){
+        int tmp = nums[index1]; 
+        nums[index1] = nums[index2];
+        nums[index2] = tmp;
+    }
+    
+    int getLeftIndexToSwap(vector<int>& nums){
+        int leftIndex = -1;
         for(int i = nums.size() - 2; i >= 0; i--){
             if(nums[i + 1] > nums[i]){
                 leftIndex = i;
@@ -11,32 +15,34 @@ public:
             }
         }
         
-        if(leftIndex != -1){
-            for(int i = nums.size() - 1; i >= 0; i--){
-                if(nums[i] > nums[leftIndex]){
-                    rightIndex = i;
-                    break;
+        return leftIndex;
+    }
+    
+    int getRightIndexToSwap(vector<int>& nums, int leftIndex){
+        int rightIndex = nums.size() - 1;
+        for(int i = nums.size() - 1; i >= 0; i--){
+            if(nums[i] > nums[leftIndex]){
+                rightIndex = i;
+                break;
             }
         }
-        int tmp = nums[leftIndex];
-        nums[leftIndex] = nums[rightIndex];
-        nums[rightIndex] = tmp;
+        return rightIndex;
+    }
+    
+    void nextPermutation(vector<int>& nums) { 
+        int leftIndex = getLeftIndexToSwap(nums);
+        int rightIndex = nums.size() - 1;
+        
+        if(leftIndex != -1){
+            rightIndex = getRightIndexToSwap(nums, leftIndex);
+            swapValues(leftIndex, rightIndex, nums);
         }
 
-        int lastIndex = nums.size() - 1;
-        
-        cout<<"left = "<<leftIndex<<" right = "<<rightIndex<<endl;
-        for(auto num: nums) cout<<num<<" ";
-        cout<<endl;
-        
-        //if(leftIndex == 0 && rightIndex == nums.size()-1) return;
-        
-        cout<<"left = "<<leftIndex<<" right = "<<rightIndex<<endl;
-            
-        for(int i = leftIndex + 1; i < nums.size() && i < lastIndex; i++){
-            int tmp = nums[lastIndex];
-            nums[lastIndex] = nums[i];
-            nums[i] = tmp;
+        int lastIndex = nums.size() - 1;  
+        leftIndex++;
+        while(leftIndex < nums.size() && leftIndex < lastIndex){
+            swapValues(leftIndex, lastIndex, nums);
+            leftIndex++;
             lastIndex--;
         }
         
