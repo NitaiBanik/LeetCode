0d4d9dcd6439041fdf8379178c32a5b98be51e5a
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-03-03 23:33:50 +0600

    Time: 4 ms (74.23%), Space: 6.6 MB (87.16%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -7,23 +7,23 @@ public:
         for(int i = 0; i < s.size(); i++)
             maxPosition[s[i]] = max(maxPosition[s[i]], i);
             
-            int cnt = 0;
-            
-            vector<int> lengths;
-           
-            int currentMaxPosition = -1;
-            
-            for(int i = 0; i < s.size(); i++){
-                cnt++;
-                currentMaxPosition = max(currentMaxPosition, maxPosition[s[i]]);
-                
-                if(i == currentMaxPosition){
-                    lengths.push_back(cnt);
-                    cnt=0;
-                    currentMaxPosition = -1;
-                }
+        int cnt = 0;
+
+        vector<int> lengths;
+
+        int currentMaxPosition = -1;
+
+        for(int i = 0; i < s.size(); i++){
+            cnt++;
+            currentMaxPosition = max(currentMaxPosition, maxPosition[s[i]]);
+
+            if(i == currentMaxPosition){
+                lengths.push_back(cnt);
+                cnt=0;
+                currentMaxPosition = -1;
             }
+        }
         
-        return lengths;
+    return lengths;
     }
 };
