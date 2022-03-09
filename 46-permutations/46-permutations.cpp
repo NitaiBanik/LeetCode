d5510135b8f39c1e6b55c98b5bc296af3c3ba6da
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-03-09 09:24:02 +0600

    Time: 542 ms (5.65%), Space: 145.4 MB (5.08%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    vector<int> permutation;
-    vector<vector<int>> allPermutation;
     
-    void generate(int mask, vector<int>& nums){
+    
+    vector<vector<int>> generate(int mask, vector<int>& nums, vector<int>& permutation, vector<vector<int>>& allPermutation){
         
         if(mask == (1<<nums.size())-1){
             allPermutation.push_back(permutation);
+            return allPermutation;
         }
+        
         for(int i = 0; i < nums.size(); i++){
             if((mask & (1<<i)) == 0){
                 permutation.push_back(nums[i]);
-                generate(mask | (1<<i), nums);
+                generate(mask | (1<<i), nums, permutation, allPermutation);
                 permutation.pop_back();
             }
         }
-        
+         return allPermutation;
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        generate(0, nums);
-        return allPermutation;
+        vector<vector<int>> allPermutation;
+        vector<int> permutation;
+        return generate(0, nums, permutation, allPermutation);
         
     }
 };
