213709556bb531860d8bb53cc308f9db1e8c1615
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-03-15 00:23:48 +0600

    Time: 12 ms (61.06%), Space: 10 MB (54.00%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
-    int maxValue = 5000000;
-    bool isWithInBoundary(int currentRow, int currentCol, int row, int col){
-        if(currentRow < 0 || currentRow == row) return false;
-        if(currentCol < 0 || currentCol == col) return false;
-        return true;
+    bool isValidIndex(int r, int c, int& row, int& col){
+        return (r>=0 && r < row && c >=0 && c < col);
     }
     
-    int fun(int currentRow, int currentCol, int row, int col, vector<vector<int>>& grid, vector<vector<int>>& dp){
-        if(currentRow == (row - 1) && currentCol == col) return 0;
-        if(currentRow == row && currentCol == (col - 1)) return 0;
-        
-        
-        if(isWithInBoundary(currentRow, currentCol, row, col) == false) return maxValue;
-        
-        if(dp[currentRow][currentCol] != -1) return dp[currentRow][currentCol];
-        
-        int val1 = grid[currentRow][currentCol] + fun(currentRow + 1, currentCol, row, col, grid, dp);
-        int val2 = grid[currentRow][currentCol] + fun(currentRow, currentCol + 1, row, col, grid, dp);
-        
-        return dp[currentRow][currentCol] = min(val1, val2);
-        
-    }
     int minPathSum(vector<vector<int>>& grid) {
-       // if(grid.size() == 0) return 0;
         
         int row = grid.size();
         int col = grid[0].size();
         
         vector<vector<int>>dp(row, vector<int>(col, -1));
         
-        return fun(0, 0, row, col, grid, dp);
+        for(int i = 0 ; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(i == 0 && j == 0)
+                    dp[i][j] = grid[i][j];
+                else{
+                    int val1 = 9999999, val2= 9999999;
+                    if(isValidIndex(i, j-1, row, col)) val1 = dp[i][j-1];
+                     if(isValidIndex(i-1, j, row, col)) val2 = dp[i-1][j];
+                    
+                    dp[i][j] = grid[i][j] + min(val1, val2);
+                }
+            }
+        }
+        
+        return dp[row-1][col-1];
         
     }
 };
