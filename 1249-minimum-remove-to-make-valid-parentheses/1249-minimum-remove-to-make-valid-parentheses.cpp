64d245c7908a9563dbfeca4b2e732cc37fbff30a
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-03-15 21:10:54 +0600

    Time: 22 ms (82.57%), Space: 10.9 MB (73.12%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -4,23 +4,25 @@ public:
         return ch >= 'a' && ch <= 'z';
     }
     string minRemoveToMakeValid(string s) {
-        
-        stack<int> indexes;
-        
-        for(int i = 0 ; i < s.size(); i++){
-            if(!isAlpha(s[i])){
-                if(s[i] == '(') indexes.push(i);
-                
-                else if(!indexes.empty() && s[indexes.top()] == '('){
-                    indexes.pop();
+        int openParentheses = 0;
+        for(int i = 0; i < s.size(); i++){
+            if(isAlpha(s[i])) continue;
+            if(s[i] == '(') openParentheses++;
+            else{
+                if(openParentheses > 0){
+                    openParentheses--;
                 }
                 else s[i] = '!';
             }
         }
-       while(!indexes.empty()){
-           s[indexes.top()] = '!';
-           indexes.pop();
-       }
+        
+        for(int i = s.size()-1; i >= 0 && openParentheses; i--){
+            if(s[i] == '('){
+                openParentheses--;
+                s[i] = '!';
+            }
+        }
+        
         string ans = "";
         for(int i = 0; i < s.size(); i++)
             if(s[i] != '!') ans+=s[i];
