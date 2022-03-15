014af951fa76a4242abc38d2991b0affab5b13c1
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-03-15 21:00:32 +0600

    Time: 42 ms (29.90%), Space: 11.5 MB (54.07%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -4,25 +4,26 @@ public:
         return ch >= 'a' && ch <= 'z';
     }
     string minRemoveToMakeValid(string s) {
-        vector<bool>isValidIndex(s.size(), false);
         
         stack<int> indexes;
         
         for(int i = 0 ; i < s.size(); i++){
-            if(isAlpha(s[i])) isValidIndex[i] = true;
-            else{
+            if(!isAlpha(s[i])){
                 if(s[i] == '(') indexes.push(i);
                 
                 else if(!indexes.empty() && s[indexes.top()] == '('){
-                    isValidIndex[i] = true;
-                    isValidIndex[indexes.top()] = true;
                     indexes.pop();
                 }
+                else s[i] = '!';
             }
         }
-        string ans ="";
+       while(!indexes.empty()){
+           s[indexes.top()] = '!';
+           indexes.pop();
+       }
+        string ans = "";
         for(int i = 0; i < s.size(); i++)
-            if(isValidIndex[i]) ans+=s[i];
+            if(s[i] != '!') ans+=s[i];
         return ans;
     }
 };
