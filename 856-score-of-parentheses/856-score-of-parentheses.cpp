586dfe66f7ba214cb931bfaa7137d5afdeb1c84d
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-03-17 19:58:58 +0600

    Time: 3 ms (30.38%), Space: 6.2 MB (8.12%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.cpp
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -3,16 +3,14 @@ public:
     int scoreOfParentheses(string s) {
         
         stack<int> st;
+        st.push(0);
         for(auto chr: s){
             if(chr == '(') st.push(0);
             else{
                 int currValue = st.top();
                 st.pop();
-                int prevValue = 0;
-                if(!st.empty()){
-                prevValue = st.top();
+                int prevValue = st.top();
                 st.pop();
-                }
                 st.push(prevValue + max(2 * currValue, 1));
             }
         }
