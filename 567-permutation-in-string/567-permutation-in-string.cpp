ca1473bd17d6842b3853859730a21882c28da0ba
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-03-18 17:24:34 +0600

    Time: 8 ms (77.51%), Space: 7.5 MB (23.01%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+     bool isSameFrequency(vector<int>& freq1, vector<int>& freq2, char ch){
+        return freq2[ch] == freq1[ch];
+    }
+    
+    int calculateFrequencyInP(vector<int>& freq1, string& p){
+        int numberOfDistinctCharInP = 0;
+        for(auto chr: p)
+        {
+            if(freq1[chr] == 0)
+                numberOfDistinctCharInP++;
+            freq1[chr]++;
+        } 
+        return numberOfDistinctCharInP;
+    }
+    
+    bool calculate(vector<int>& freq1, vector<int>& freq2, string& s, int numberOfDistinctCharInP){
+        int lf = 0, numberOfDistinctCharInS = 0;
+        for(int i = 0; i < s.size() && lf < s.size(); i++){
+            freq2[s[i]]++;
+            
+            if(isSameFrequency(freq1, freq2, s[i]))
+                numberOfDistinctCharInS++;
+
+            while(lf < s.size() && lf <= i && freq2[s[i]] > freq1[s[i]]){
+                 if(freq2[s[lf]] == freq1[s[lf]]) numberOfDistinctCharInS--;
+                freq2[s[lf++]]--;
+
+            }
+            if(numberOfDistinctCharInS == numberOfDistinctCharInP){
+                return true;
+            }
+        }
+        return false;
+    }
+    bool checkInclusion(string s1, string s2) {
+         vector<int> freq1(126, 0);
+        vector<int> freq2(126, 0);
+        int numberOfDistinctCharInP = 0;
+        
+        numberOfDistinctCharInP = calculateFrequencyInP(freq1, s1);    
+        return calculate(freq1, freq2, s2, numberOfDistinctCharInP);
+    }
+};
