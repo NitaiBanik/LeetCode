cc02b00f2845452bdcc03f63960adfa9a66addfa
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-03-18 16:49:33 +0600

    Time: 21 ms (54.86%), Space: 8.8 MB (35.67%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
+    bool isSameFrequency( vector<int>& freq2,  vector<int>& freq1, char ch){
+        return freq2[ch] == freq1[ch];
+    }
     vector<int> findAnagrams(string s, string p) {
         vector<int> freq1(126, 0);
         vector<int> freq2(126, 0);
         int numberOfDistinctCharInP = 0;
-         int numberOfDistinctCharInS = 0;
+        int numberOfDistinctCharInS = 0;
         
         for(int i = 0; i < p.size(); i++)
         {
@@ -18,7 +21,9 @@ public:
         for(int i = 0; i < s.size() && lf < s.size(); i++){
             freq2[s[i]]++;
             
-            if(freq2[s[i]] == freq1[s[i]]) numberOfDistinctCharInS++;
+            if(isSameFrequency(freq2,freq1, s[i]))
+                numberOfDistinctCharInS++;
+            
             if(freq2[s[i]] > freq1[s[i]]){
                 while(lf < s.size() && lf <= i && freq2[s[i]] > freq1[s[i]]){
                      if(freq2[s[lf]] == freq1[s[lf]]) numberOfDistinctCharInS--;
