f867304529ff7d45214b42a397766b45320c1bb6
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-03-18 19:54:12 +0600

    Time: 300 ms (33.63%), Space: 62.6 MB (8.72%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    map<pair<int,int>, int>mp;
-    int ways(vector<int>& nums, int pos, int target){
+    int ways(vector<int>& nums, int pos, int target, map<pair<int,int>, int> &mp){
         if(pos == nums.size()){
             if(target == 0) return 1;
             return 0;
         }
         if(mp.find({pos, target}) != mp.end()) return mp[{pos, target}];
         
-        return mp[{pos, target}] = ways(nums, pos + 1, target - nums[pos])
-            +  ways(nums, pos + 1, target + nums[pos]);
+        return mp[{pos, target}] = ways(nums, pos + 1, target - nums[pos], mp)
+            +  ways(nums, pos + 1, target + nums[pos], mp);
     }
+    
     int findTargetSumWays(vector<int>& nums, int target) {
-        return ways(nums, 0, target);
+        map<pair<int,int>, int>mp;
+        return ways(nums, 0, target, mp);
     }
 };
