5c6bed9d20765b79ae8affa204b18f80a98a2a4a
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-03-18 17:05:32 +0600

    Time: 15 ms (74.40%), Space: 8.7 MB (35.67%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -1,40 +1,49 @@
 class Solution {
 public:
-    bool isSameFrequency( vector<int>& freq2,  vector<int>& freq1, char ch){
+    bool isSameFrequency(vector<int>& freq1, vector<int>& freq2, char ch){
         return freq2[ch] == freq1[ch];
     }
-    vector<int> findAnagrams(string s, string p) {
-        vector<int> freq1(126, 0);
-        vector<int> freq2(126, 0);
+    
+    int calculateFrequencyInP(vector<int>& freq1, string& p){
         int numberOfDistinctCharInP = 0;
-        int numberOfDistinctCharInS = 0;
-        
-        for(int i = 0; i < p.size(); i++)
+        for(auto chr: p)
         {
-            if(freq1[p[i]] == 0) numberOfDistinctCharInP++;
-            freq1[p[i]]++;
-        }
-        
-        int lf = 0;
-        vector<int>positions;
-        
+            if(freq1[chr] == 0)
+                numberOfDistinctCharInP++;
+            freq1[chr]++;
+        } 
+        return numberOfDistinctCharInP;
+    }
+    
+    void calculate(vector<int>& freq1, vector<int>& freq2, string& s, int numberOfDistinctCharInP, vector<int>& positions){
+        int lf = 0, numberOfDistinctCharInS = 0;
         for(int i = 0; i < s.size() && lf < s.size(); i++){
             freq2[s[i]]++;
             
-            if(isSameFrequency(freq2,freq1, s[i]))
+            if(isSameFrequency(freq1, freq2, s[i]))
                 numberOfDistinctCharInS++;
-            
-            if(freq2[s[i]] > freq1[s[i]]){
-                while(lf < s.size() && lf <= i && freq2[s[i]] > freq1[s[i]]){
-                     if(freq2[s[lf]] == freq1[s[lf]]) numberOfDistinctCharInS--;
-                    freq2[s[lf++]]--;
-                    
-                }
+
+            while(lf < s.size() && lf <= i && freq2[s[i]] > freq1[s[i]]){
+                 if(freq2[s[lf]] == freq1[s[lf]]) numberOfDistinctCharInS--;
+                freq2[s[lf++]]--;
+
             }
             if(numberOfDistinctCharInS == numberOfDistinctCharInP){
                 positions.push_back(lf);
             }
         }
+        
+    }
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> freq1(126, 0);
+        vector<int> freq2(126, 0);
+        int numberOfDistinctCharInP = 0;
+        
+        numberOfDistinctCharInP = calculateFrequencyInP(freq1, p);
+
+        vector<int>positions;     
+        calculate(freq1, freq2, s, numberOfDistinctCharInP, positions);
+        
         return positions;
     }
 };
