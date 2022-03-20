3f79b1025e02394a06b6f37a12d527e736f6efdc
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-03-20 19:41:31 +0600

    Time: 108 ms (95.45%), Space: 111.6 MB (29.27%) - LeetHub

---
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+        
+        int up = tops[0];
+        int down = bottoms[0];
+        int x = 0, y = 0;
+        
+        for(int i = 0; i < tops.size(); i++){
+            if(tops[i] == up || bottoms[i] == up) x++;
+            if(tops[i] == down || bottoms[i] == down) y++;
+        }
+        
+        if(x != tops.size() && y != bottoms.size()) return -1;
+        x = 0, y = 0;
+        
+        for(int i = 0; i < tops.size(); i++){
+            if(tops[i] != up) x++;
+             if(bottoms[i] != down) y++;
+        }
+        swap(up, down);
+        int mn = 999999;
+        
+        mn =  min(x,y);
+        
+        x = 0, y = 0;
+        
+        for(int i = 0; i < tops.size(); i++){
+            if(tops[i] != up) x++;
+             if(bottoms[i] != down) y++;
+        }
+        
+         mn =  min(mn, min(x,y));
+        
+        return mn;
+    }
+};
