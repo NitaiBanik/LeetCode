c3378529c8449d9d6e33a1edd35fa1edec0fa1cb
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-03-25 23:20:06 +0600

    Time: 8 ms (42.77%), Space: 8 MB (47.20%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
-    int dp[101][51][51];
-    int fun(vector<vector<int>>& costs, int index, int group1, int group2){
-        if(index == costs.size()) return 0;
-        if(dp[index][group1][group2] != -1) return dp[index][group1][group2];
-        
-        if(group2 == 0)  return dp[index][group1][group2] = costs[index][0] + fun(costs, index + 1, group1 - 1, 0);
-        if(group1 == 0)  return dp[index][group1][group2] = costs[index][1] + fun(costs, index + 1, 0, group2 - 1);
-        else return dp[index][group1][group2] =  min(costs[index][0] + fun(costs, index + 1, group1 - 1, group2), costs[index][1] + fun(costs, index + 1, group1, group2 - 1));
-        
-    }
+   
     int twoCitySchedCost(vector<vector<int>>& costs) {
         
-        memset(dp, -1, sizeof(dp));
+      priority_queue<pair<int,int>>pq;
+      int n = costs.size(), sum = 0;
         
-        return fun(costs, 0, costs.size() / 2, costs.size() / 2);
+        for(int i = 0; i < n; i++){
+            pq.push({-(costs[i][0] - costs[i][1]),i});
+        }
+        
+        n /= 2;
+        while(!pq.empty()){
+            if(!n){
+                sum += costs[pq.top().second][1];
+            }
+            else{
+                  sum += costs[pq.top().second][0];
+                n--;
+            }
+            pq.pop();
+        }
+        
+        return sum;
     }
 };
