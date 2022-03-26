73be90984831225fcb88aca174619262c4a51c3a
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-03-26 21:57:48 +0600

    Time: 80 ms (86.29%), Space: 41.7 MB (25.58%) - LeetHub

---
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        
+        unordered_map<int, int> frequencyOfNumber;
+        frequencyOfNumber[0] = 1;
+        
+        int sum = 0, count = 0;
+        
+        for(auto num: nums){
+            sum += num;
+            count += (frequencyOfNumber[sum - k]);
+            
+            frequencyOfNumber[sum]++;
+        }
+        return count;
+    }
+};
