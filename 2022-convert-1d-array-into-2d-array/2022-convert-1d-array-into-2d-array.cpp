59c27fd624b00985229b0211a7878982cbf6d22e
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-03-26 19:53:09 +0600

    Time: 214 ms (26.72%), Space: 85.3 MB (82.38%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
@@ -3,10 +3,13 @@ public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
         if(original.size() != (m * n)) return {};
         
-        vector<vector<int>> vec(m);
+        vector<vector<int>> vec(m, vector<int>(n));
+        
+        int row = 0, col = 0;
         
         for(int i = 0; i < original.size(); i++){
-            vec[i/n].push_back(original[i]);
+            vec[row][col++] = original[i];
+                   if(col == n) row++, col = 0;
         }
         
         return vec;
