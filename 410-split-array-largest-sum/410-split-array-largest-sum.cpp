854cf83fbfdc1814ad9b6baac666dca614f1d79c
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-03-31 22:02:47 +0600

    Time: 1147 ms (6.81%), Space: 12.7 MB (7.04%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -2,42 +2,40 @@ class Solution {
 public:
     int dp[1002][1002];
     
-    int fun(vector<int>& nums, int pos, int remain){
+    int fun(vector<int>& nums, vector<vector<int>>& dp, int pos, int remain){
         if(pos == nums.size()-1){
             if(remain == 0) return 0;
-            else return 2147483647;
+            else return INT_MAX;
         }
          if(remain == 0){
             if(pos == nums.size()-1) return 0;
-            else return 2147483647;
+            else return INT_MAX;
         }
         if(dp[pos][remain] != -1) return dp[pos][remain];
         
         int mn = INT_MAX;
         
         for(int i = pos + 1; i < nums.size(); i++){
-            int mx = max(nums[i] - nums[pos], fun(nums, i, remain-1));
+            int mx = max(nums[i] - nums[pos], fun(nums, dp, i, remain-1));
             mn = min(mn, mx);
         }
         
         return dp[pos][remain] = mn;
     }
     int splitArray(vector<int>& nums, int m) {
-        memset(dp, -1, sizeof(dp));
         nums.push_back(0);
+        
+        vector<vector<int>>dp(nums.size(), vector<int>(m + 1, -1));
         
         for(int i = nums.size() - 1; i >= 1; i--)
             nums[i] = nums[i-1];
         
-         nums[0] = 0;
+        nums[0] = 0;
         
             
        for(int i = 1; i < nums.size(); i++)
-            nums[i] += nums[i-1];
+           nums[i] += nums[i-1];
         
-        for(int i = 0; i < nums.size(); i++)
-           cout<<nums[i]<<" ";
-        
-        return fun(nums, 0, m);
+        return fun(nums, dp, 0, m);
     }
 };
