c1730364d99e47ed2ac41c9a2f2f0c8461395611
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-04-02 00:08:31 +0600

    Time: 56 ms (98.27%), Space: 19.9 MB (86.95%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -3,8 +3,8 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         
         if(nums.size() < 3) return {};
-        set<vector<int>>setOfAnswer;
-        vector<vector<int>>ans;
+        
+        vector<vector<int>>setOfAnswer;
         
         sort(nums.begin(), nums.end());
         
@@ -15,17 +15,27 @@ public:
             
             while(start < end){
                 int currentSum = nums[start] + nums[end];
+                
                 if(currentSum == need){
-                    setOfAnswer.insert({nums[i], nums[start], nums[end]});
-                    start++, end--;
+                    setOfAnswer.push_back({nums[i], nums[start], nums[end]});
+                    
+                    int l = nums[start], r = nums[end];
+                    while((nums[start] == l) && start < end)
+                        start++;
+                    
+                     while((nums[end] == r) && start < end)
+                         end--;
                 }
+                
                 else if(currentSum > need) end--;
                 else start++;
             }
+            
+            int val = nums[i];
+            while((i + 1 < nums.size()) && (val == nums[i + 1]))
+                i++;
         }
         
-        for(auto val: setOfAnswer)
-            ans.push_back(val);
-        return ans;
+        return setOfAnswer;
     }
 };
