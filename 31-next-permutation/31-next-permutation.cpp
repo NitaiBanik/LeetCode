43b949ac3650e84a5fb921216d2cdceeaeaade05
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-04-03 10:47:02 +0600

    Time: 0 ms (100.00%), Space: 12.2 MB (37.25%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,50 +1,30 @@
 class Solution {
 public:
-    void swapValues(int index1, int index2, vector<int>& nums){
-        int tmp = nums[index1]; 
-        nums[index1] = nums[index2];
-        nums[index2] = tmp;
-    }
-    
-    int getLeftIndexToSwap(vector<int>& nums){
-        int leftIndex = -1;
-        for(int i = nums.size() - 2; i >= 0; i--){
-            if(nums[i + 1] > nums[i]){
-                leftIndex = i;
+    void nextPermutation(vector<int>& nums) {
+        
+        if(nums.size() < 2) return;
+        
+        int lf = -1;
+        int rt = nums.size() - 1;
+        for(int i = nums.size() - 1; i > 0; i--){
+            if(nums[i] > nums[i - 1])
+            {
+                lf = i - 1;
                 break;
             }
         }
         
-        return leftIndex;
-    }
-    
-    int getRightIndexToSwap(vector<int>& nums, int leftIndex){
-        int rightIndex = nums.size() - 1;
-        for(int i = nums.size() - 1; i >= 0; i--){
-            if(nums[i] > nums[leftIndex]){
-                rightIndex = i;
-                break;
+        cout<<lf<<" "<<rt<<endl;
+        sort(nums.begin() + lf + 1, nums.end());
+        if(lf != -1){
+            for(int i = lf + 1; i <= rt; i++){
+                if(nums[i] > nums[lf])
+                {
+                    rt = i; 
+                    break;
+                }
             }
+            swap(nums[lf], nums[rt]);
         }
-        return rightIndex;
-    }
-    
-    void nextPermutation(vector<int>& nums) { 
-        int leftIndex = getLeftIndexToSwap(nums);
-        int rightIndex = nums.size() - 1;
-        
-        if(leftIndex != -1){
-            rightIndex = getRightIndexToSwap(nums, leftIndex);
-            swapValues(leftIndex, rightIndex, nums);
-        }
-
-        int lastIndex = nums.size() - 1;  
-        leftIndex++;
-        while(leftIndex < nums.size() && leftIndex < lastIndex){
-            swapValues(leftIndex, lastIndex, nums);
-            leftIndex++;
-            lastIndex--;
-        }
-        
     }
 };
