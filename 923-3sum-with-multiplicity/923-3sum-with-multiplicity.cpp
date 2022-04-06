1c5e5e1c3729d44ec12744aa2e8529a49e717452
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-04-06 22:40:50 +0600

    Time: 1110 ms (24.49%), Space: 10.3 MB (90.82%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -11,17 +11,19 @@ public:
         
         for(int i = 0; i < arr.size() - 2; i++){
             vec[arr[i]]--;
+            int pos = i + 1;
             for(int j = i + 1; j < arr.size() -1; j++){
                 vec[arr[j]]--;
+                pos = j;
                 
                 int need = target - (arr[i] + arr[j]);
-                if(need < 0) continue;
+                if(need < 0) break;
                 ans += vec[need];
                 if(ans >= mod)
                 ans %= mod;
             }
             
-            for(int j = i + 1; j < arr.size() -1; j++)
+            for(int j = i + 1; j <= pos; j++)
                 vec[arr[j]]++;
         }
         return ans;
