0d0a2b051ed0dc26cfb789cb4d2bf848ce786bec
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-04-06 23:57:52 +0600

    Time: 264 ms (58.16%), Space: 10.5 MB (74.83%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -2,28 +2,49 @@ class Solution {
 public:
     int threeSumMulti(vector<int>& arr, int target) {
         int mod = 1000000000 + 7;
-        vector<int>vec(301, 0);
         
-        for(auto val: arr)
-            vec[val]++;
+        sort(arr.begin(), arr.end());
+        
         long long ans = 0;
         
         for(int i = 0; i < arr.size() - 2; i++){
-            vec[arr[i]]--;
-            int pos = i + 1;
-            for(int j = i + 1; j < arr.size() -1; j++){
-                vec[arr[j]]--;
-                pos = j;
-                
-                int need = target - (arr[i] + arr[j]);
-                if(need < 0) continue;
-                ans += vec[need];
-                if(ans >= mod)
-                ans %= mod;
-            }
+            int T = target - arr[i];
             
-            for(int j = i + 1; j <= pos; j++)
-                vec[arr[j]]++;
+            int  lf = i + 1, rt = arr.size() - 1;
+            
+            while(lf < rt){
+                if(arr[lf] + arr[rt] > T)
+                    rt--;
+                else if(arr[lf] + arr[rt] < T)
+                    lf++;
+                else if(arr[lf] != arr[rt]){
+
+                    int x = 1, y = 1;
+                    while(lf + 1 < rt && arr[lf] == arr[lf + 1]){
+                        lf++;
+                        x++;
+                    }
+                    
+                     while(rt - 1 > lf && arr[rt] == arr[rt - 1]){
+                        rt--;
+                        y++;
+                     }
+                    
+                    ans += (x * y);
+                    ans %= mod;
+                    
+                    lf++;
+                    rt--;
+                }
+                else{
+                    long long ln = (rt - lf + 1);
+                    ln *= (ln - 1);
+                    
+                    ans += (ln / 2);
+                    ans %= mod;   
+                    break;
+                }
+            }
         }
         return ans;
     }
