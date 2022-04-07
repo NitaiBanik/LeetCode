dc3442311d18c094d3e4aaf6f78b8bb64764dcd8
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-04-07 19:38:29 +0600

    Time: 365 ms (30.24%), Space: 95.5 MB (17.51%) - LeetHub

---
+++ 1546-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target/1546-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxNonOverlapping(vector<int>& nums, int target) {
+        
+        int sum = 0, lf = 0, cnt = 0;
+        map<int, bool>mp;
+        mp[0] = true;
+        for(int i = 0; i < nums.size(); i++){
+            sum  += nums[i];
+            
+            if(mp.find(sum - target) != mp.end()){
+                cnt++;
+                mp.clear();
+                sum = 0;
+            }
+           mp[sum] = true;
+        }
+        return cnt;
+    }
+};
