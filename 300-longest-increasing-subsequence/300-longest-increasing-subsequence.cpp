4e5ca5458ee3b72f80189879693c68faba9ab113
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-04-07 20:44:51 +0600

    Time: 8 ms (93.81%), Space: 10.6 MB (15.24%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         
-        vector<int>lis;
-        
-        lis.push_back(nums[0]);
+        vector<int>lis(nums.size(), 10004);
+         
+         lis[0] = nums[0];
+         int pos = 0;
         
         for(int i = 1; i < nums.size(); i++){
-            if(lis.back() < nums[i])
-                lis.push_back(nums[i]);
+            if(lis[pos] < nums[i]){
+                pos++;
+                 lis[pos] = nums[i];
+            }
             else{
                 int positionOfEqualImmidiateGraterValue = lower_bound(lis.begin
 (), lis.end(), nums[i]) - lis.begin();
@@ -16,6 +19,6 @@ public:
             }
         }
         
-        return lis.size();
+        return pos + 1;
     }
 };
