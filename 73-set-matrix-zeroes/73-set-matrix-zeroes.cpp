45bfae2e281f1889304395124e71c343ea6cf570
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-04-12 20:52:59 +0600

    Time: 26 ms (23.89%), Space: 13.3 MB (53.64%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        //int row = 0, col = 0;
         
-        vector<bool> rows(matrix.size(), 0);
-        vector<bool> cols(matrix[0].size(), 0);
+        vector<int> rows(7, 0);
+        vector<int> cols(7, 0);
         
         for(int i = 0;i < matrix.size(); i++)
             for(int j = 0; j < matrix[0].size(); j++){
                 if(matrix[i][j] == 0){
-                    rows[i] = 1;
-                    cols[j] = 1;
-                   // row |= (1 << i);
-                   // col |= (1<<j);
+                   rows[i/30] |= (1 << (i%30));
+                   cols[j/30] |= (1 << (j%30));
                 }
             }
         
          for(int r = 0; r < matrix.size(); r++){
-             int val = rows[r];
+             int val = rows[r/30] & (1 << (r%30));
              if(val)
              for(int c = 0; c < matrix[0].size(); c++)
                  if(val) matrix[r][c] = 0;
          }
         
          for(int c = 0; c < matrix[0].size(); c++){
-             int val = cols[c];
+              int val = cols[c/30] & (1 << (c%30));
              if(val)
             for(int r = 0; r < matrix.size(); r++)
                 if(val) matrix[r][c] = 0;
