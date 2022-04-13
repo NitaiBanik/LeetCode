62cc4fe2e0985b14305e4b668714e4227f126cab
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-04-13 22:09:35 +0600

    Time: 17 ms (66.35%), Space: 12.3 MB (28.04%) - LeetHub

---
+++ 885-spiral-matrix-iii/885-spiral-matrix-iii.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    bool isValid(int rows, int cols, int r, int c){
+        return (r >= 0 && r < rows) && (c >= 0 && c < cols);
+    }
+    vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        
+        int r = 1, d = 1, l = 2, u = 2;
+        
+        int tot = rows * cols, cnt = 1;
+        
+        
+        vector<vector<int>> ans;
+        ans.push_back({rStart, cStart});
+        if(cnt == tot) return ans;
+        
+        while(1){
+            for(int i = 0; i < r; i++){
+                cStart++;
+                
+                if(isValid(rows, cols, rStart, cStart)){
+                    cnt++;
+                    ans.push_back({rStart, cStart});
+                    if(cnt == tot) return ans;
+                }
+            }
+            
+            for(int i = 0; i < d; i++){
+                rStart++;
+                
+                 if(isValid(rows, cols, rStart, cStart)){
+                    cnt++;
+                    ans.push_back({rStart, cStart});
+                    if(cnt == tot) return ans;
+                }
+            }
+            
+            for(int i = 0; i < l; i++){
+                cStart--;
+                
+                 if(isValid(rows, cols, rStart, cStart)){
+                    cnt++;
+                    ans.push_back({rStart, cStart});
+                    if(cnt == tot) return ans;
+                }
+            }
+            
+            for(int i = 0; i < u; i++){
+                rStart--;
+                
+               if(isValid(rows, cols, rStart, cStart)){
+                    cnt++;
+                    ans.push_back({rStart, cStart});
+                    if(cnt == tot) return ans;
+                }
+            }
+            r += 2, d += 2, l += 2, u +=2;
+        }
+        return ans;
+    }
+};
