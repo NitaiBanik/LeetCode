465e02ca81f40213b14d62fe029927e41fd4c0ed
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-04-23 14:42:59 +0600

    Time: 0 ms (100.00%), Space: 6.6 MB (74.39%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -6,7 +6,7 @@ public:
     string encode(string longUrl) {
        // cout<<"agge = long"<<longUrl<<endl;
         for(int i = 0; i < longUrl.size(); i++)
-            longUrl[i] = (char)longUrl[i] - 'z';
+            longUrl[i] = (char)longUrl[i] - '`';
       //  cout<<"pore long = "<<longUrl<<endl;
         return longUrl;
     }
@@ -16,7 +16,7 @@ public:
        // cout<<"agge short = "<<shortUrl<<endl;
         
          for(int i = 0; i < shortUrl.size(); i++)
-            shortUrl[i] = (char)shortUrl[i] + 'z';
+            shortUrl[i] = (char)shortUrl[i] + '`';
         
        //  cout<<"pore short = "<<shortUrl<<endl;
         return shortUrl;
