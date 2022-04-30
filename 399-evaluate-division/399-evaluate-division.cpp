1252b24682f68fde578eba5fb54ed545c8e178de
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-05-01 00:34:45 +0600

    Time: 0 ms (100.00%), Space: 8.2 MB (89.56%) - LeetHub

---
+++ 399-evaluate-division/399-evaluate-division.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    bool found;
+    double value;
+    
+    void dfs(map<string, vector<string>>& graph, map<string, vector<double>>& cost, map<string, bool>& visited, string from, string to, double currentCost){
+        
+        if(from == to){
+            found = true;
+            value = currentCost;
+        }
+        
+        if(found) return;
+        
+        visited[from] = true;
+        
+        for(int i = 0; i < graph[from].size(); i++){
+            if(visited[graph[from][i]]) continue;
+            if(found) return;
+            dfs(graph, cost, visited, graph[from][i], to, currentCost * cost[from][i]);
+            
+        }
+    }
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        
+        map<string, vector<string>> graph;
+        map<string, vector<double>> cost;  
+        map<string, bool> visited;
+        
+        
+        for(int i = 0; i < equations.size(); i++){
+            string u = equations[i][0];
+            string v = equations[i][1];
+            
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+            
+            cost[u].push_back(values[i]);
+            cost[v].push_back((double)1.00/values[i]);
+        }
+        
+        vector<double> ans;
+            
+        for(int i = 0; i < queries.size(); i++){
+            string from = queries[i][0];
+            string to = queries[i][1];
+            
+            if(graph.find(from) == graph.end() || graph.find(to) == graph.end()){
+                ans.push_back(-1.00);
+            }       
+            else{
+                found = false;
+                visited.clear();
+                
+               dfs(graph, cost, visited, from, to, 1.00);
+                if(found)
+                ans.push_back(value);
+                else  ans.push_back(-1.00);
+            }
+            
+        }
+        
+        return ans;
+        
+    }
+};
