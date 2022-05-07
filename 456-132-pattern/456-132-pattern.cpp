9fd6c291a03b7b2eac412e7413c3ecbfc9ca08ae
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-05-07 23:24:37 +0600

    Time: 86 ms (68.24%), Space: 39.3 MB (27.15%) - LeetHub

--- 456-132-pattern/456-132-pattern.cpp
+++ 456-132-pattern/456-132-pattern.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
         vector<int>leftMin(nums.size(), 0);
-        set<long long>stt;
+        int mnLeft = nums[0];
         
-        int mnLeft = INT_MAX, mnRight = INT_MAX;
-        
-        for(int i = 0, j = nums.size() - 1; i < nums.size(); i++, j--){
+        for(int i = 0; i < nums.size(); i++){
             leftMin[i] = mnLeft;
-         //   rightMin[j] = mnRight;
             mnLeft = min(mnLeft, nums[i]);
-         //   mnRight = min(mnRight, nums[j]);
         }
         
-        for(int i = nums.size() -1; i >= 0; i--){
-            auto lowerBound = stt.lower_bound((long long)leftMin[i] + 1);
+        stack<int> valuesGreaterThanMin;      
+        for(int i = nums.size() - 1; i > 0; i--){
             
-            if(lowerBound != stt.end() && leftMin[i] < nums[i] && nums[i] > *lowerBound)
-                return true;
-            stt.insert(nums[i]);
+            while(!valuesGreaterThanMin.empty() && valuesGreaterThanMin.top() <= leftMin[i])
+                valuesGreaterThanMin.pop();
+            
+            if(!valuesGreaterThanMin.empty() && valuesGreaterThanMin.top() < nums[i])
+                 return true;
+            
+            valuesGreaterThanMin.push(nums[i]);
         }
         
         return false;
