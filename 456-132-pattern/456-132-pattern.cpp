847c494b6ec892e97bef507d9e85e0aea4e719ca
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-05-08 00:18:48 +0600

    Time: 97 ms (51.43%), Space: 38.2 MB (68.61%) - LeetHub

--- 456-132-pattern/456-132-pattern.cpp
+++ 456-132-pattern/456-132-pattern.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
-        vector<int>leftMin(nums.size(), 0);
-        int mnLeft = nums[0];
         
-        for(int i = 0; i < nums.size(); i++){
-            leftMin[i] = mnLeft;
-            mnLeft = min(mnLeft, nums[i]);
-        }
+        int thirdValue = INT_MIN;
+        stack<int> stak;
         
-        stack<int> valuesGreaterThanMin;      
-        for(int i = nums.size() - 1; i > 0; i--){
+        for(int i = nums.size() - 1; i >= 0; i--){
+            if(nums[i] < thirdValue) return true;
             
-            while(!valuesGreaterThanMin.empty() && valuesGreaterThanMin.top() <= leftMin[i])
-                valuesGreaterThanMin.pop();
-            
-            if(!valuesGreaterThanMin.empty() && valuesGreaterThanMin.top() < nums[i])
-                 return true;
-            
-            valuesGreaterThanMin.push(nums[i]);
+            while(!stak.empty() && stak.top() < nums[i]){
+                thirdValue = stak.top();
+                stak.pop();
+            }
+            stak.push(nums[i]);
         }
         
         return false;
