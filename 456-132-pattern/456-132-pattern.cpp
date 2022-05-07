d6ff6150a60881f5f9367b969bd574af181bf26f
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-05-07 22:21:39 +0600

    Time: 247 ms (7.76%), Space: 55.5 MB (7.39%) - LeetHub

---
+++ 456-132-pattern/456-132-pattern.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        vector<int>leftMin(nums.size(), 0);
+        set<long long>stt;
+        
+        int mnLeft = INT_MAX, mnRight = INT_MAX;
+        
+        for(int i = 0, j = nums.size() - 1; i < nums.size(); i++, j--){
+            leftMin[i] = mnLeft;
+         //   rightMin[j] = mnRight;
+            mnLeft = min(mnLeft, nums[i]);
+         //   mnRight = min(mnRight, nums[j]);
+        }
+        
+        for(int i = nums.size() -1; i >= 0; i--){
+            auto lowerBound = stt.lower_bound((long long)leftMin[i] + 1);
+            
+            if(lowerBound != stt.end() && *lowerBound > leftMin[i] && leftMin[i] < nums[i] && nums[i] > *lowerBound)
+                return true;
+            stt.insert(nums[i]);
+        }
+        
+        return false;
+        
+    }
+};
