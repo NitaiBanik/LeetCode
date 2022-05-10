2bb1059f461cba42bcb8addae9ce6d3dd603ca04
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-05-10 07:47:45 +0600

    Time: 0 ms (100.00%), Space: 6.7 MB (37.26%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,25 +1,24 @@
 class Solution {
-public:
-    vector<int>numbers;    
-    vector<vector<int>> answers;
-    
-    void fun(int k, int n, int usedNumber){
+public:    
+    void fun(int k, int n, int usedNumber, vector<int>&path, vector<vector<int>>& answers){
         if(k == 0)
         {
-            if(n == 0) answers.push_back(numbers);
+            if(n == 0) answers.push_back(path);
             return;
         }
         
         if(n < 0 || usedNumber > 9) return;
         
-        numbers.push_back(usedNumber);
-        fun(k-1, n - usedNumber, usedNumber + 1);
-        numbers.pop_back();
-        fun(k, n, usedNumber + 1);
+        path.push_back(usedNumber);
+        fun(k-1, n - usedNumber, usedNumber + 1, path, answers);
+        path.pop_back();
+        fun(k, n, usedNumber + 1, path, answers);
     }
     
     vector<vector<int>> combinationSum3(int k, int n) {
-        fun(k, n, 1);
+        vector<int>path;
+        vector<vector<int>> answers;
+        fun(k, n, 1, path, answers);
         return answers;
     }
 };
