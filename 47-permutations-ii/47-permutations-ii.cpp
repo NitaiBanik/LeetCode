ed2d07665011a5bd8dc260e80367873591a31222
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-05-12 23:52:24 +0600

    Time: 9 ms (67.44%), Space: 8.7 MB (75.71%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,44 +1,40 @@
 class Solution {
 public:
-     bool nextPermutation(vector<int>& nums) {
-        
-        if(nums.size() < 2) return false;
+    vector<int> nextPermutation(vector<int>& nums){
+        if(nums.size() < 2) return {};
         
         int lf = -1;
-        int rt = nums.size() - 1;
-        for(int i = nums.size() - 1; i > 0; i--){
-            if(nums[i] > nums[i - 1])
-            {
+        
+        for(int i = nums.size() - 1; i> 0; i--){
+            if(nums[i] > nums[i-1]){
                 lf = i - 1;
                 break;
             }
         }
-         
-        if(lf == -1) return false;
-        sort(nums.begin() + lf + 1, nums.end());
-        if(lf != -1){
-            for(int i = lf + 1; i <= rt; i++){
-                if(nums[i] > nums[lf])
-                {
-                    rt = i; 
-                    break;
-                }
+        
+        if(lf == -1) return {};
+        sort(nums.begin() + lf + 1,nums.end());
+        
+        for(int i = lf + 1; i < nums.size(); i++){
+            if(nums[i] > nums[lf]){
+                swap(nums[i], nums[lf]);
+                break;
             }
-            swap(nums[lf], nums[rt]);
-        }
-        return true;
-    }
-    
-    vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<vector<int>> ans;
-        sort(nums.begin(), nums.end());
-        while(1){
-         ans.push_back(nums);
-           bool isExists = nextPermutation(nums);
-            if(!isExists)
-               break;
         }
         
-        return ans;
+        return nums;
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        
+        vector<vector<int>> answer;
+        
+        sort(nums.begin(), nums.end());
+        
+        while(1){
+            answer.push_back(nums);
+            nums = nextPermutation(nums);
+            if(nums.size() == 0) break;
+        }
+        return answer;
     }
 };
