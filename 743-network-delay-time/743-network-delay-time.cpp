7d63fa352c6e4b3bdcc3a184b8a488a43c7ae0e4
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-05-14 14:16:46 +0600

    Time: 277 ms (14.20%), Space: 42 MB (47.72%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,41 +1,44 @@
 class Solution {
+    int maximumPossibleCost = 6000 * 6000; 
 public:
-    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<int> cost(n + 1, INT_MAX);
+    vector<vector<int>> buildGraphFromInput(int nodes, vector<vector<int>>& graph, vector<vector<int>>& times){
+        for(int node = 1; node <= nodes; node++)
+            graph[node][node] = 0;
         
-        vector<pair<int, int>> graph[n + 1];
         for(auto edge: times){
-            graph[edge[0]].push_back({edge[1], edge[2]});
+            graph[edge[0]][edge[1]] = edge[2];
+        }    
+        
+        return graph;
+    }
+    
+    vector<vector<int>> computeDelayTimesByFloydWarshal(int nodes, vector<vector<int>>& graph){
+        for(int middle = 1; middle <= nodes; middle++)
+            for(int from = 1; from <= nodes; from++)
+                for(int to = 1; to <= nodes; to++)
+                    graph[from][to] = min(graph[from][to], graph[from][middle] + graph[middle][to]);
+        
+        return graph;
+    }
+    
+    int getMaxTime(int nodes, int startNode, vector<vector<int>>&graph){ 
+        int maxTime = INT_MIN;
+        for(int node = 1; node <= nodes; node++){
+            if(graph[startNode][node] == maximumPossibleCost) return -1;
+            
+            maxTime = max(maxTime, graph[startNode][node]);
         }
         
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> >Q;
+        return maxTime;
+    }
+    
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {    
+        vector<vector<int>>graph(n + 1, vector<int>(n + 1, maximumPossibleCost));
         
-        Q.push({k, 0});
-        cost[k] = 0;
+        graph = buildGraphFromInput(n, graph, times);
+        graph = computeDelayTimesByFloydWarshal(n, graph);
         
-        while(!Q.empty()){
-            pair<int, int> top = Q.top();
-            Q.pop();
-            for(auto edge: graph[top.first]){
-                if(cost[edge.first] > edge.second + top.second){
-                    cost[edge.first] =  edge.second + top.second;
-                    Q.push({edge.first, cost[edge.first]});
-                }
-            }
-        }
-        
-        int totalVisited = 0;
-        for(int i = 1; i<= n; i++)
-            totalVisited += (cost[i] != INT_MAX);
-        
-        int mx = INT_MIN;
-        for(int i = 1; i<= n; i++){
-            //cout<<"node = "<<i<<" cost = "<<cost[i]<<endl;
-            mx = max(mx, cost[i]);
-        }
-        
-        
-        return totalVisited == n ? mx: -1;
+        return getMaxTime(n, k, graph);
         
     }
 };
