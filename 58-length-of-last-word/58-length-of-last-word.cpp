07492538fc077f48c6a3fbbe0d594ae921f8d411
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-05-21 00:18:08 +0600

    Time: 5 ms (24.42%), Space: 6.5 MB (52.03%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.cpp
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
-    bool isAlpah(char& ch){
-        if(ch >= 'a' & ch <= 'z') return true;
-        if(ch >= 'A' & ch <= 'Z') return true;
-        else return false;
-    }
     int lengthOfLastWord(string s) {
-        int last = s.size() - 1;
+        int last = s.size() - 1;  
+        int cnt = 0, isFound = false;
         
-        while(last >= 0 && !isAlpah(s[last]))
-            last--;
-        
-        int cnt = 0;
-        while(last >= 0 && isAlpah(s[last])){
-            cnt++;
-            last--;
+        for(int i = last; i>=0; i--){
+            if(s[i] == ' ' && isFound == true)
+                break;
+            if(s[i] != ' ')
+                isFound = true;
+            
+            if(isFound) cnt++;
         }
         
         return cnt;
