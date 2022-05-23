f366ae3c2d1117ba04a0dd65ee77f3d9ead6ac96
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-05-24 02:11:06 +0600

    Time: 536 ms (29.58%), Space: 35.7 MB (47.01%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int dp[601][101][101];
-    vector<pair<int, int>> arr;
+    vector<pair<int, int>> oneTwos;
     int fun(int pos, int m, int n){
-        if(pos == arr.size()) return 0;
+        if(pos == oneTwos.size()) return 0;
         
         if(dp[pos][m][n] != -1) return dp[pos][m][n];
         
         int mx = 0;
         
-        if(m - arr[pos].first >= 0  && n - arr[pos].second >= 0)
-        mx = max(mx, 1 + fun(pos + 1, m - arr[pos].first, n - arr[pos].second ));
+        if(m - oneTwos[pos].first >= 0  && n - oneTwos[pos].second >= 0)
+        mx = max(mx, 1 + fun(pos + 1, m - oneTwos[pos].first, n - oneTwos[pos].second ));
         mx = max(mx, fun(pos + 1, m, n));
         
         return dp[pos][m][n] = mx;
@@ -18,9 +18,13 @@ public:
     
     int findMaxForm(vector<string>& strs, int m, int n) {
         
-        for(auto s: strs){
-            int zero=count(s.begin(),s.end(),'0');
-            arr.push_back({zero, s.size() - zero});
+        for(int i = 0; i < strs.size(); i++){
+            int one = 0, zero = 0;
+            for(int j = 0; j < strs[i].size(); j++){
+                if(strs[i][j] == '1') one++;
+                else zero++;
+            }
+            oneTwos.push_back({zero, one});
         }
         
         memset(dp, -1, sizeof(dp));
