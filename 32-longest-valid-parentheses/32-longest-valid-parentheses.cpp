a66c43dd838b276f5c5145f9674526dc4cddafcf
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-05-24 11:00:04 +0600

    Time: 0 ms (100.00%), Space: 7.4 MB (42.55%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        int mx = 0;
+        
+        int currentSum = 0, mx = 0;
         stack<int>st;
         st.push(-1);
         
-        
         for(int i = 0; i < s.size(); i++){
-            if(s[i] == '(') st.push(i);
+            if(s[i] =='(')
+                st.push(i);
             else{
                 st.pop();
-                if(st.empty()) st.push(i);
-                else mx = max(mx, i - st.top());
+                if(st.empty()) st.push(i);     
+                mx =  max(mx, i - st.top());
             }
         }
-        
         return mx;
-        
     }
 };
