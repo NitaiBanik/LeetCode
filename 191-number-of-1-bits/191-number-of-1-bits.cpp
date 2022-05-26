2b3dda6878ff2814f329935e24edb21a8aa2bc86
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-05-26 09:59:20 +0600

    Time: 0 ms (100.00%), Space: 6 MB (48.74%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
-    // n & (n - 1) flips the last 1's bit. We will count How many operations required to make n ---> 0
     int hammingWeight(uint32_t n) {
-        
-        int totalOnes = 0;
-            
-            while(n != 0){
-                n = n & (n - 1);
-                totalOnes++;
-            }
-        
-        return totalOnes;
-        
+        int cnt = 0;
+        while(n!=0){
+            cnt += (n%2LL == 1);
+            n/=2;
+        }
+        return cnt;
     }
 };
