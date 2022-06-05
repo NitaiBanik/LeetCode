04978b3730446da204327f0c55892ae13413e44c
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-06-05 14:37:44 +0600

    Time: 6 ms (64.32%), Space: 6.2 MB (46.51%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
 
  int ans = 0;
-    bool isValid(vector<string>& board, int row, int col){
+    bool isValid(vector<vector<bool>>& board, int row, int col){
         for(int i = row; i >= 0; i--)
-            if(board[i][col] =='Q') return false;
+            if(board[i][col]) return false;
         
         for(int i = row, j = col; i >= 0 && j >= 0; i--, j--)
-            if(board[i][j] =='Q') return false;
+            if(board[i][j]) return false;
         
          for(int i = row, j = col; i >= 0 && j < board.size(); i--, j++)
-            if(board[i][j] =='Q') return false;
+            if(board[i][j]) return false;
         
         return true;
     }
-    void solveBoard(vector<string> &board, int row){
+    void solveBoard(vector<vector<bool>> &board, int row){
         if(row == board.size()){
             ans++;
             return;
@@ -22,14 +22,14 @@ public:
         
         for(int col = 0; col < board.size(); col++){
             if(isValid(board, row, col)){
-                board[row][col] = 'Q';
+                board[row][col] = true;
                 solveBoard(board, row + 1);
-                board[row][col] = '.';
+                board[row][col] = false;
             }
         }
     }
     int totalNQueens(int n) {
-        vector<string> board(n, string(n, '.'));
+        vector<vector<bool>> board(n, vector<bool>(n, false));
         solveBoard(board, 0);
         
     return ans;
