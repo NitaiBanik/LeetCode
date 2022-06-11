64c2db922e119486368c48fd7bc0f0f9479eb15e
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-06-12 01:37:34 +0600

    Time: 1318 ms (5.04%), Space: 218.2 MB (5.04%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
-        int lf = 0;
-        long sum = 0, curr = 0;
-        int mn = 0;
+        map<int, int> suffix;
+        int sum = 0, mn = 999999999;
         
-        for(int i = 0; i < nums.size(); i++){
-            sum += nums[i];
-        }
-      
-        sum = sum - x;
-        if(sum == 0) return nums.size();   
-        if(sum < 0) return -1;
-        
-        for(int i = 0; i < nums.size(); i++)
+        for(int i = 0 ; i < nums.size(); i++)
         {
-            curr += nums[i];
-            while(curr > sum)
-                curr -= nums[lf++];
-            if(curr == sum)
-                mn = max(mn, i - lf + 1);
-        }    
-        
-      return mn == 0 ? -1: nums.size() - mn; 
+            sum += nums[i];
+            suffix[sum] = i + 1;
+            if(sum == x)
+                 mn = min(mn, i+1);
+                
+        }
+        sum = 0;
+        for(int i = nums.size() - 1; i >=0; i--){
+            sum += nums[i];
+            if(sum == x)
+                mn = min(mn, (int)nums.size() - i);
+            
+            if(suffix[x- sum] > 0 && i + 1 > suffix[x- sum] )
+                mn = min(mn, suffix[x - sum] + (int)nums.size() - i);
+        }
+        return mn == 999999999 ? -1: mn;
     }
+    
 };
