bfbe3929f66aab093618bdb8e2af34183f824b1e
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-06-27 12:22:07 +0600

    Time: 118 ms (44.34%), Space: 40.3 MB (36.79%) - LeetHub

---
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int MinPartitions(string n) {
+        int sum = 0, mx = 0;
+        
+        for(int i = n.Length - 1; i >= 0; i--){
+            int value = n[i]-'0';
+            
+            if(value <= mx) continue;
+            
+            sum += (value- mx);
+            mx = Math.Max(mx, value);
+        }
+        return  sum;
+    }
+}
