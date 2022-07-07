2f834a6edd6c1dc164e8444076a01fd4a4795ee4
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-07-08 02:10:50 +0600

    Time: 4 ms (74.12%), Space: 6.2 MB (96.27%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int dp[105][105];
-    
-    bool fun(int x, int y,string s1, string s2, string s3){
+    string s3, s1, s2;
+    bool fun(int x, int y){
         if(x == s1.size() && y == s2.size())
             return 1;
         if(dp[x][y] != -1) return dp[x][y];
@@ -10,18 +10,20 @@ public:
         int ans = 0;
         
         if(s1[x] == s3[x + y])
-            ans |= fun(x+1, y, s1, s2, s3);
+            ans |= fun(x+1, y);
         
          if(s2[y] == s3[x + y])
-            ans |= fun(x, y + 1, s1, s2, s3);
+            ans |= fun(x, y + 1);
         
         return dp[x][y]= ans;
     }
-    bool isInterleave(string s1, string s2, string s3) {
-        
+    bool isInterleave(string s11, string s22, string s33) {
+        s3 = s33;
+        s1 = s11;
+        s2 =  s22;
         if(s1.size() + s2.size() != s3.size()) return false;
         memset(dp, -1, sizeof(dp));
         
-        return fun(0, 0, s1, s2, s3);
+        return fun(0, 0);
     }
 };
