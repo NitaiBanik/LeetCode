6fd4e31d371914369e74493667770e10dbb00752
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-07-08 02:09:12 +0600

    Time: 40 ms (27.50%), Space: 33.4 MB (16.94%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     int dp[105][105];
     
-    bool fun(int x, int y, int z, string s1, string s2, string s3){
+    bool fun(int x, int y,string s1, string s2, string s3){
         if(x == s1.size() && y == s2.size())
             return 1;
         if(dp[x][y] != -1) return dp[x][y];
         
         int ans = 0;
         
-        if(s1[x] == s3[z])
-            ans |= fun(x+1, y, z + 1, s1, s2, s3);
+        if(s1[x] == s3[x + y])
+            ans |= fun(x+1, y, s1, s2, s3);
         
-         if(s2[y] == s3[z])
-            ans |= fun(x, y + 1, z + 1, s1, s2, s3);
+         if(s2[y] == s3[x + y])
+            ans |= fun(x, y + 1, s1, s2, s3);
         
         return dp[x][y]= ans;
     }
@@ -22,6 +22,6 @@ public:
         if(s1.size() + s2.size() != s3.size()) return false;
         memset(dp, -1, sizeof(dp));
         
-        return fun(0, 0, 0, s1, s2, s3);
+        return fun(0, 0, s1, s2, s3);
     }
 };
