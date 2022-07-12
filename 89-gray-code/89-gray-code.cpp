6e02259b2e605286d1f76d605ad0d6f30cec1ad1
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-07-13 00:55:09 +0600

    Time: 16 ms (49.13%), Space: 12.8 MB (32.17%) - LeetHub

--- 89-gray-code/89-gray-code.cpp
+++ 89-gray-code/89-gray-code.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> fun(int pos){
-        if(pos == 1) return {0, 1};
+        if(pos == 0) return {0, 1};
         
         vector<int> result;
         vector<int>temp = fun(pos-1);
@@ -9,11 +9,11 @@ public:
         result = temp;
         
         for(int i = temp.size() - 1; i >=0; i--)
-            result.push_back(temp[i] | (1 << (pos-1)));
+            result.push_back(temp[i] | (1 << pos));
         
         return result;
     }
     vector<int> grayCode(int n) {
-          return fun(n);
+          return fun(n-1);
     }
 };
