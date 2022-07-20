e1f622ded80c998063e9c0ca7dacf6db8992b93c
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-07-20 13:12:12 +0600

    Time: 303 ms (64.68%), Space: 51.6 MB (43.75%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -3,12 +3,12 @@ public:
     bool isFound(string s, vector<vector<int>>& characterIndecies){
         int foundAt = -1; 
         for(int i = 0; i < s.size(); i++){
-            auto position = upper_bound(characterIndecies[s[i]-'a'].begin(), characterIndecies[s[i]-'a'].end(), foundAt);    
+            auto position = lower_bound(characterIndecies[s[i]-'a'].begin(), characterIndecies[s[i]-'a'].end(), foundAt);    
 
             if(position == characterIndecies[s[i]-'a'].end())
                 return false;
 
-            foundAt = *position;
+            foundAt = *position + 1;
         }
         return true;
     }
