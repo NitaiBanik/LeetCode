a43706f3253e03dec2692ae23bea09f1db98b2f6
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-08-01 22:42:43 +0600

    Time: 2 ms (50.13%), Space: 6.1 MB (63.21%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
+        vector<int> previousRow(n, 1), currentRow(n, 1);
         
-        vector<vector<int>> path(m, vector<int>(n, 0));
-      
-        for(int i = 0; i < n; i++){
-            path[0][i] = 1;
-        }
-        
-        for(int i = 0; i < m; i++){
-            path[i][0] = 1;
-        }
-        
-         for(int i = 1; i < m; i++)
+         for(int i = 1; i < m; i++){
              for(int j = 1; j < n; j++){
-                     path[i][j] = path[i-1][j] + path[i][j-1];
+                 currentRow[j] = previousRow[j] + currentRow[j-1];
              }
+             swap(previousRow, currentRow);
+         }
         
-        return path[m-1][n-1];
+        return previousRow[n-1];
     }
 };
