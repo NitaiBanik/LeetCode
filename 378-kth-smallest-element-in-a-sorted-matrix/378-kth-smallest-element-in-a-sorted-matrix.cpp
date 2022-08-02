3ce6737b39a4604e6f1012a0dea6d5295a379c8f
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-08-02 23:13:20 +0600

    Time: 37 ms (85.07%), Space: 13.2 MB (77.54%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -5,15 +5,15 @@ public:
         int size = matrix.size();
         int lo = matrix[0][0], hi = INT_MAX/2;
         
-        while(lo < hi){
-            int mid = lo + (hi-lo) / 2;
+        while(lo <= hi){
+            int mid = (lo + hi) / 2;
             int cnt = 0;
             for(int i = 0; i < size; i ++)
                 cnt += upper_bound(matrix[i].begin(), matrix[i].end(), mid) - matrix[i].begin();
             
             if(cnt < k)
                 lo = mid+1;
-            else hi = mid;
+            else hi = mid - 1;
         }
         return lo;
     }
