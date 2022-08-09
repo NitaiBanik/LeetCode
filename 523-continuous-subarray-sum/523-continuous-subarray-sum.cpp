dda9940a518c25989554c1289bc88b605df01364
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-08-09 21:26:30 +0600

    Time: 284 ms (55.86%), Space: 115 MB (6.40%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.cpp
@@ -8,11 +8,13 @@ public:
         
         for(int i = 0; i < nums.size(); i++){
             sum += nums[i];
-            if(k)sum %= k;
+            if(k != 0)sum %= k;
             
             if(mp.find(sum) != mp.end()){
-                if(i - mp[sum] > 1){ cout<<sum<<endl;
-                                    return true;}
+                if(i - mp[sum] > 1){
+                    // cout<<sum<<endl;
+                    return true;
+                }
             }
             else
             mp[sum] = i;
