7a9043307729fd44a98254917442290b1a4bc84a
Nitai Chandra Banik <nitaibanik29@gmail.com>
2022-10-20 01:50:56 +0600

    Time: 23 ms (60.23%), Space: 12.6 MB (77.02%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    static bool comp(pair<int, string>& p1, pair<int, string>& p2){
+        if(p1.first == p2.first)
+            return p1.second < p2.second;
+        
+        return p1.first > p2.first;
+    }
+    
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        map<string, int> mp;
+        for(auto word: words)
+            mp[word]++;
+        
+        vector<pair<int, string>> pairs;
+        
+        for(auto mpp: mp)
+            pairs.push_back({mpp.second, mpp.first});
+        
+        sort(pairs.begin(), pairs.end(), comp);
+        
+        vector<string> answer;
+        
+        for(int i = 0; i < k; i++)
+            answer.push_back(pairs[i].second);
+        
+        return answer;
+    }
+};
