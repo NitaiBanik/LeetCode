ea7c0378e928a3b350ea5d17988396dcdc922230
Nitai Chandra Banik <nitaibanik29@gmail.com>
2023-05-13 18:37:35 +0600

    Time: 16 ms (66.48%), Space: 9.4 MB (81.95%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
-    long long fun(int pos, int low, int high, int zero, int one, vector<int>&dp){
-        if(pos > high) return 0;
-        
-       if(dp[pos] != -1) return dp[pos];
-        
-        long long ans = 0;
-        
-        if(pos >= low && pos <= high) ans = 1LL;
-        
-        ans += fun(pos + zero, low, high, zero, one, dp);
-        ans += fun(pos + one, low, high, zero, one, dp);
-        ans %= 1000000007;
-        
-       // cout<<pos<<" ans = "<<ans<<endl;
-        
-        return dp[pos] = ans;
-    }
     int countGoodStrings(int low, int high, int zero, int one) {
-        vector<int>dp(high + 1, -1);
+        vector<int>dp(high + 1);
         
-        return fun(0, low, high, zero, one, dp);
+        dp[0] = 1;
+        
+        for(int i = 0; i <= high; i++){
+           if(i >= zero) dp[i] += dp[i - zero] % 1000000007;
+           if(i >= one) dp[i] += dp[i - one] % 1000000007;
+        }
+        
+        long long ans = 0LL;
+         for(int i = low; i <= high; i++)
+             ans = (ans + dp[i]) % 1000000007;;
+        
+        return ans;
     }
 };
