f36c79e111400ca0634044c75931a696d219d058
Nitai Chandra Banik <nitaibanik29@gmail.com>
2023-06-13 21:57:46 +0700

    Time: 644 ms (6.85%), Space: 23.3 MB (92.90%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
-        map<vector<int>, int> mp;
         int ans = 0;
-        
-        for(auto vec:  grid)
-            mp[vec]++;
-        
         for(int i = 0; i < grid[0].size(); i++){
-            vector<int> col;
             for(int j = 0; j < grid[0].size(); j++){
-                col.push_back(grid[j][i]);
+                bool isSame = true;
+                
+                for(int k = 0; k <grid[0].size(); k++){
+                    if(grid[k][j] != grid[i][k]){
+                        isSame = false;
+                        break;
+                    }
+                }
+                 ans += isSame;
             }
-            ans += mp[col];
         }
         
         return ans;
