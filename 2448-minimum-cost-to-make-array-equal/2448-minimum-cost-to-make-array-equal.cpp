b3d71ef72e9192f62ece644da4b39cd363c335c9
Nitai Chandra Banik <nitaibanik29@gmail.com>
2023-06-21 21:45:26 +0700

    Time: 131 ms (29.56%), Space: 38.4 MB (71.17%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    long long getAns(long long mid, vector<int>& nums, vector<int>& cost){
-        long long tot = 0LL;
+    pair<long long, long long> getAns(long long mid1, long long mid2, vector<int>& nums, vector<int>& cost){
+        long long tot1 = 0LL, tot2 = 0LL;
         
         for(int i = 0; i < nums.size(); i++){
-            tot += (abs(mid - nums[i]) * cost[i] * 1LL);
+            tot1 += (abs(mid1 - nums[i]) * cost[i] * 1LL);
+            tot2 += (abs(mid2 - nums[i]) * cost[i] * 1LL);
         }
         
-        return tot;
+        return {tot1, tot2};
     }
     long long minCost(vector<int>& nums, vector<int>& cost) {
-        long long lo = 1LL, hi = 1000000LL;
+        long long lo = 1LL, hi = 1000000LL, ans1 = 0LL, ans2 = 0LL;
         
         while(lo <= hi){
-            long long mid = (lo + hi) / 2.00;
-            long long ans1 = getAns(mid, nums, cost);
-            long long ans2 = getAns(mid + 1, nums, cost);
+            long long mid = (lo + hi) / 2LL;
+            auto ans = getAns(mid, mid+1, nums, cost);
+            ans1 = ans.first;
+            ans2 = ans.second;
             
             if(ans1 > ans2){
                 lo = mid + 1;
@@ -24,6 +26,6 @@ public:
             }
         }
         
-        return getAns(lo, nums, cost);
+        return min(ans1, ans2);
     }
 };
