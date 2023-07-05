cbfc3356c43eed1ca8b0dd69bde48716872b407b
Nitai Chandra Banik <nitaibanik29@gmail.com>
2023-07-06 00:45:52 +0700

    Time: 32 ms (97.95%), Space: 36.4 MB (65.98%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        vector<int> vec;
-        int cn = 0;
+        int cn = 0, ans = 0, prev = 0;
         for(auto n: nums){
             if(n == 1) cn++;
             else{
-                vec.push_back(cn);
-                if(cn != 0)     vec.push_back(0);
+               ans = max(ans, cn + prev);
+                prev = cn;
                 cn = 0;
             }
         }
-        
-        if(cn > 0) vec.push_back(cn);
-        
-        if(vec.size() == 1) return vec[0] - 1 < 0 ? 0 :  vec[0] - 1;
-        
-        cn = 0;
-       // for(auto x: vec) cout<<x<<endl;
-          for(int i = 0; i < vec.size(); i++){
-              if(vec[i] == 0){
-                  int x = i > 0 ? vec[i-1] : 0;
-                  int y = i < vec.size() - 1 ? vec[i + 1] : 0;
-                  
-                  cn = max(cn,  x  + y);
-              }
-          }
-        
-        return cn;
+        if(cn == nums.size()) cn--;
+        ans = max(ans, cn + prev);
+        return ans;
     }
 };
